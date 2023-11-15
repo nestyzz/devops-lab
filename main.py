bb1bad8185556c01b633ba8b101e9e13669b18ab
nestyzz <zelandn77@gmail.com>
2023-11-15 22:15:53 +0500

    add some changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+print('add some changes to this file')
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
